20322185ce835d86172d7c5f57ca1e9931da58b6
erblack <erblack@tripleten.com>
2023-12-27 18:00:47 +0100

    Default description file for init

--- bin/init.js
+++ bin/init.js
@@ -8,8 +8,9 @@ const program = new Command();
 program
     .description('Generate a description file for a package.json')
     .option('-f, --force [force]', 'Replace existed file', Boolean)
-    .action(({ force }) => {
-        return init(force);
+    .option('-d, --default-descriptions [defaultDescriptions]', 'Default descriptions file path', String)
+    .action(({ force, defaultDescriptions }) => {
+        return init(force, defaultDescriptions);
     });
 
 await program.parseAsync(process.argv);
