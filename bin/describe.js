94ca427831ed6e31b4f002c85250d4293e398525
erblack <erblack@tripleten.com>
2023-12-27 16:01:14 +0100

    Describe unknown packages

--- bin/describe.js
+++ bin/describe.js
@@ -1,12 +1,22 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
+import { Command, Option } from 'commander';
 import { describe } from '../lib/tasks/describe.js';
 
 const program = new Command();
 
-program.description('Describe new packages').action(() => {
-    return describe();
-});
+program
+    .description('Describe new packages')
+    .addOption(
+        new Option(
+            '-t, --type <type>',
+            'Which packages to describe. new – not described yet, unknown – with status unknown, all – both of them',
+        )
+            .choices(['new', 'unknown', 'all'])
+            .default('new'),
+    )
+    .action(({ type }) => {
+        return describe(type);
+    });
 
 await program.parseAsync(process.argv);
